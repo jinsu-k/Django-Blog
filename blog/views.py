fc2dfb56813fb7cc53a19df89f6451b339eb9357
jinsu-l <wlstn8738@naver.com>
2020-07-12 18:38:00 +0900

    blog nav 글쓰기추가, new.html추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Blog
 
 def home(request):
@@ -8,3 +9,17 @@ def home(request):
 def detail(request,blog_id):
     blog_detail = get_object_or_404(Blog,pk=blog_id) 
     return render(request, 'detail.html',{'blog':blog_detail})
+
+#new.html을 띄워주는 함수
+def new(request):
+    return render(request, 'new.html')
+
+#입력받은 내용을 데이터베이스에 넣어주는 함수
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    #blog.id는 int형 이기때문에 str로 강제 형변환
+    return redirect('/blog/'+str(blog.id))
